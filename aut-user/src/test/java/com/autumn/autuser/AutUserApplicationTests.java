331ebb4ba8d56d2cbcc24a8a215aa781c094f3c7
秋晚晚呀 <3418013863@qq.com>
2024-11-13 17:55:49 +0800

    新增aut-user

---
+++ aut-user/src/test/java/com/autumn/autuser/AutUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autumn.autuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
